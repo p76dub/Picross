b78594f8ab12cce2abd88b6464746c38ab080c1f
Pierre DUBAILLAY <pierredubaillay@orange.fr>
2017-08-18 16:22:10 +0200

    Création de l'interface Grid

---
+++ src/model/Grid.java
@@ -0,0 +1,63 @@
+import java.awt.Dimension;
+
+/**
+ * Les grilles permettent de d'arranger des objets sous forme de grille. Chaque objet est associé à une coordonnée dans
+ * grille. Il est possible d'interroger une grille pour connaître l'objet à une position donnée.
+ * @param <T> le type des objets contenus dans la grille
+ * @inv <pre>
+ *     getWidth() == getSize().getWidth()
+ *     getHeight() == getSize().getHeight()
+ *     getSize() != null
+ *     getSize().getWidth() >= 0
+ *     getSize().getHeight() >= 0
+ * </pre>
+ */
+public interface Grid<T> {
+    // REQUETES
+
+    /**
+     * La taille de la grille.
+     * @return une Dimension
+     */
+    Dimension getSize();
+
+    /**
+     * La longueur de la grille.
+     * @return un entier positif
+     */
+    int getWidth();
+
+    /**
+     * La hauteur de la grille.
+     * @return un entier positif
+     */
+    int getHeight();
+
+    /**
+     * Donne l'élément en position x, y.
+     * @param x l'ordonnée de l'élément
+     * @param y l'abscisse de l'élément
+     * @return un objet de type T
+     * @pre <pre>
+     *     0 <= x < getWidth()
+     *     0 <= y < getHeight()
+     * </pre>
+     */
+    T getObjectAt(int x, int y);
+
+    // COMMANDES
+    /**
+     * Permet de changer l'objet stocké dans la grille en position (x, y).
+     * @pre <pre>
+     *      0 <= x < getWidth()
+     *      0 <= y < getHeight()
+     * </pre>
+     * @post <pre>
+     *     getObjectAt(x, y) == o
+     * </pre>
+     * @param x l'ordonnée de l'élément
+     * @param y l'abscisse de l'élément
+     * @param o : l'objet à placer
+     */
+    void setObjectAt(int x, int y, T o);
+}
